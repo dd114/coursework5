e0d828e1b133e66f4f729fe1a2b758b1968a67db
dd114 <40179423+dd114@users.noreply.github.com>
2021-12-18 21:18:49 +0500

    add code and MNIST's files (not working)

--- src/huch/company/Main.java
+++ src/huch/company/Main.java
@@ -1,7 +1,23 @@
 package huch.company;
 
+import huch.MNIST.MnistDataReader;
+import huch.MNIST.MnistMatrix;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        MnistMatrix[] mnistMatrix = new MnistDataReader().readData("data/train-images.idx3-ubyte", "data/train-labels.idx1-ubyte");
+        printMnistMatrix(mnistMatrix[mnistMatrix.length - 1]);
+        mnistMatrix = new MnistDataReader().readData("data/t10k-images.idx3-ubyte", "data/t10k-labels.idx1-ubyte");
+        printMnistMatrix(mnistMatrix[0]);
+
+
+
+
+
+
         //NeuralNetwork(кол-во слоев и нейронов в них, [скорость обучения], [продуктивность])
         MNN network = new MNN(new int[][]{{2}, {7}, {4}}, 0.1f, 0.3f);
 
@@ -23,6 +39,17 @@ public class Main {
 
 //        System.out.println(network.predict(new float[]{1, 1, 1}));
 
+    }
+
+
+    private static void printMnistMatrix(final MnistMatrix matrix) {
+        System.out.println("label: " + matrix.getLabel());
+        for (int r = 0; r < matrix.getNumberOfRows(); r++ ) {
+            for (int c = 0; c < matrix.getNumberOfColumns(); c++) {
+                System.out.print(matrix.getValue(r, c) + " ");
+            }
+            System.out.println();
+        }
     }
 }
 
