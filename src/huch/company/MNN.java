c82d10b00d917bdb2514e6cad79ffa271560c6f5
dd114 <40179423+dd114@users.noreply.github.com>
2021-12-19 15:01:06 +0500

    modification: change MNN file according with new formulas gradient descent recognition working isn't correct

--- src/huch/company/MNN.java
+++ src/huch/company/MNN.java
@@ -7,19 +7,19 @@ public class MNN {
     private float moment = 0.3f;
 
     public float[][][] overallLayer;
-    private float[][][] deltaOverallLayer;
+    //private float[][][] deltaOverallLayer;
     private float[][] error_neuron;
 
     public MNN(int[][] layer) {
         overallLayer = new float[layer.length][0][0];
         overallLayer[0] = new float[layer[0][0]][0];
 
-        deltaOverallLayer = new float[layer.length][0][0];
-        deltaOverallLayer[0] = new float[layer[0][0]][0];
+//        deltaOverallLayer = new float[layer.length][0][0];
+//        deltaOverallLayer[0] = new float[layer[0][0]][0];
 
         for (int i = 1; i < layer.length; i++) {
             overallLayer[i] = new float[layer[i][0]][overallLayer[i - 1].length + 1];
-            deltaOverallLayer[i] = new float[layer[i][0]][overallLayer[i - 1].length + 1];
+//            deltaOverallLayer[i] = new float[layer[i][0]][overallLayer[i - 1].length + 1];
         }
 
         for (int i = 1; i < overallLayer.length; i++)
@@ -37,6 +37,118 @@ public class MNN {
     public MNN(int[][] layer, float learning_rate, float moment) {
         this(layer, learning_rate);
         this.moment = moment;
+    }
+
+    public int predict(float[] inputs) {
+        float[][] inputs_layer = new float[overallLayer.length][0];
+        float[][] outputs_layer = new float[overallLayer.length][0];
+        float max = 0;
+        int answer = -1;
+
+        outputs_layer[0] = inputs;
+
+        for (int i = 1; i < overallLayer.length; i++) {
+            inputs_layer[i] = multiplyByMatrix(overallLayer[i], outputs_layer[i - 1]);
+            outputs_layer[i] = activationMapper(inputs_layer[i]);
+        }
+
+//        for (float b : outputs_layer[outputs_layer.length - 1])  System.out.println(b); // выводим сигнал каждого выхода (результат каждого аутпута)
+
+        for (int i = 0; i < outputs_layer[outputs_layer.length - 1].length - 1; i++) {
+            System.out.println(i + " = " + outputs_layer[outputs_layer.length - 1][i]);
+        }
+
+        // находим максимальный из них, который и будет являться ответом
+        for (int i = 0; i < outputs_layer[outputs_layer.length - 1].length - 1; i++)
+            if (outputs_layer[outputs_layer.length - 1][i] > max) {
+                max = outputs_layer[outputs_layer.length - 1][i];
+                answer = i;
+            }
+        return answer; //выводим номер нейрона соответствующему определенному ответу
+    }
+
+    public void train(float[] inputs, int[] correct_predict) {
+        float[][] inputs_layer = new float[overallLayer.length][0];
+        float[][] outputs_layer = new float[overallLayer.length][0];
+        error_neuron = new float[overallLayer.length][0];
+        outputs_layer[0] = inputs;
+
+        for (int i = 1; i < overallLayer.length; i++) {
+            inputs_layer[i] = multiplyByMatrix(overallLayer[i], outputs_layer[i - 1]);
+            outputs_layer[i] = activationMapper(inputs_layer[i]);
+        }
+
+        int last_layer = overallLayer.length - 1;
+
+
+        for (int i = 0; i < last_layer; i++)
+            error_neuron[i] = new float[overallLayer[i].length + 1]; //заполняем массив количеством нейронов, +1 - нейрон смещения
+        error_neuron[last_layer] = new float[overallLayer[last_layer].length]; //заполняем последний слой массива без нейрона смещения
+
+        //считаем ошибку всех нейронов на последнем скрытом слое
+        for (int i = 0; i < overallLayer[last_layer].length; i++) // определяем ошибку для последнего слоя по формуле correct_answer - expected_answer
+            error_neuron[last_layer][i] = correct_predict[i] - outputs_layer[last_layer][i];
+
+        // тк ошибки нет на первом входном слое => i >= 1, а i = last_layer - 1, тк последний слой заполнили выше, ошибка также расставлется и на нейрон смещения
+        for (int i = last_layer - 1; i >= 1; i--)
+            for (int j = 0; j < error_neuron[i].length; j++) { // смотрим каждый нейрон слоя i
+                for (int k = 0; k < overallLayer[i + 1].length; k++) { // смотрим каждый нейрон слоя i + 1
+                    //метод обратного распостранения ошибки, умножаем ошибку на вес
+                    error_neuron[i][j] += error_neuron[i + 1][k] * overallLayer[i + 1][k][j];
+                }
+                // затем сумму умножаем на производную функции активации и получаем итоговую ошибку
+                error_neuron[i][j] = error_neuron[i][j] * derivativeActivation(inputs_layer[i][j]);
+            }
+
+
+        for (int i = 1; i < overallLayer.length; i++) // добавить нейрон смещения и дельту весов
+            for (int j = 0; j < overallLayer[i].length; j++) {
+                for (int k = 0; k < overallLayer[i][j].length - 1; k++) {
+                    //по формуле произведения производной сигмоиды, на выход предыдущего слоя и ошибки плюс изменение весов (дельта весов) на предыдущей итерации помноженое на момент
+                    overallLayer[i][j][k] += learning_rate * error_neuron[i][j] * outputs_layer[i - 1][k] * derivativeActivation(inputs_layer[i][j]);
+                    //запоминаем их тут, чтобы использовать на следующей итерации
+//                    deltaOverallLayer[i][j][k] = learning_rate * error_neuron[i][j] * outputs_layer[i][j] * (1 - outputs_layer[i][j]) * outputs_layer[i - 1][k];
+                }
+                // корректируем весы нейрона смещения
+                overallLayer[i][j][overallLayer[i][j].length - 1] += learning_rate * error_neuron[i][j] * 1 * derivativeActivation(inputs_layer[i][j]);
+                //запоминаем их тут, чтобы использовать на следующей итерации
+//                deltaOverallLayer[i][j][deltaOverallLayer[i][j].length - 1] = learning_rate * error_neuron[i][j] * outputs_layer[i][j] * (1 - outputs_layer[i][j]) * 1;
+            }
+    }
+
+    private float[] sigmoidMapper(float[] inputs) {
+        float[] outputs = new float[inputs.length];
+        for (int i = 0; i < inputs.length - 1; i++) outputs[i] = sigmoid(inputs[i]);
+        outputs[inputs.length - 1] = 1;
+        return outputs;
+    }
+
+    private float sigmoid(float x) {
+        return (float) (1 / (1 + Math.exp(-x)));
+    }
+
+    private float activation(float x){
+        return sigmoid(x);
+    }
+
+    private float[] activationMapper(float[] inputs){
+        return sigmoidMapper(inputs);
+    }
+
+    private float derivativeActivation(float x){
+        float ePow = (float) Math.exp(-x);
+        return ePow / ((ePow + 1) * (ePow + 1));
+    }
+
+    private float[] multiplyByMatrix(float[][] m1, float[] m2) { //2 version with neuron bias, 1 version above
+        float[] overallM = new float[m1.length + 1];
+        overallM[m1.length] = 1;
+        for (int y = 0; y < m1.length; y++) {
+            assert m1[y].length - 1 == m2.length;
+            for (int x = 0; x < m1[y].length - 1; x++) overallM[y] += m1[y][x] * m2[x];
+            overallM[y] += m1[y][m1[y].length - 1] * 1;
+        }
+        return overallM;
     }
 
     public void save(String path) {
@@ -59,6 +171,7 @@ public class MNN {
             e.printStackTrace();
         }
     }
+
     public void error(String path) {
         float sum_err = 0;
         try {
@@ -71,99 +184,6 @@ public class MNN {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public int predict(float[] inputs) {
-        float[][] inputs_layer = new float[overallLayer.length][0];
-        float[][] outputs_layer = new float[overallLayer.length][0];
-        float max = 0;
-        int answer = -1;
-
-        outputs_layer[0] = inputs;
-
-        for (int i = 1; i < overallLayer.length; i++) {
-            inputs_layer[i] = multiplyByMatrix(overallLayer[i], outputs_layer[i - 1]);
-            outputs_layer[i] = sigmoid_mapper(inputs_layer[i]);
-        }
-
-//        for (float b : outputs_layer[outputs_layer.length - 1])  System.out.println(b); // выводим сигнал каждого выхода (результат каждого аутпута)
-
-        for (int i = 0; i < outputs_layer[outputs_layer.length - 1].length; i++) {
-            System.out.println(i + " = " + outputs_layer[outputs_layer.length - 1][i]);
-        }
-
-        // находим максимальный из них, который и будет являться ответом
-        for (int i = 0; i < outputs_layer[outputs_layer.length - 1].length; i++)
-            if (outputs_layer[outputs_layer.length - 1][i] > max) {
-                max = outputs_layer[outputs_layer.length - 1][i];
-                answer = i;
-            }
-        return answer; //выводим номер нейрона соответствующему определенному ответу
-    }
-
-    public void train(float[] inputs, int[] correct_predict) {
-        float[][] inputs_layer = new float[overallLayer.length][0];
-        float[][] outputs_layer = new float[overallLayer.length][0];
-        error_neuron = new float[overallLayer.length][0];
-        outputs_layer[0] = inputs;
-
-        for (int i = 1; i < overallLayer.length; i++) {
-            inputs_layer[i] = multiplyByMatrix(overallLayer[i], outputs_layer[i - 1]);
-            outputs_layer[i] = sigmoid_mapper(inputs_layer[i]);
-        }
-
-
-        for (int i = 0; i < overallLayer.length - 1; i++)
-            error_neuron[i] = new float[overallLayer[i].length + 1]; //заполняем массив количеством нейронов, +1 - нейрон смещения
-        error_neuron[overallLayer.length - 1] = new float[overallLayer[overallLayer.length - 1].length]; //заполняем последний слой массива без нейрона смещения
-
-        int last_layer = overallLayer.length - 1;
-        //считаем ошибку всех нейронов на последнем скрытом слое
-        for (int i = 0; i < overallLayer[last_layer].length; i++) // определяем ошибку для последнего слоя по формуле correct_answer - expected_answer
-            error_neuron[last_layer][i] = correct_predict[i] - outputs_layer[last_layer][i];
-
-        // тк ошибки нет на первом входном слое => i >= 1, а i = last_layer - 1, тк последний слой заполнили выше, ошибка также расставлется и на нейрон смещения
-        for (int i = last_layer - 1; i >= 1; i--)
-            for (int j = 0; j < error_neuron[i].length; j++) { // смотрим каждый нейрон слоя i
-                for (int k = 0; k < overallLayer[i + 1].length; k++) { // смотрим каждый нейрон слоя i + 1
-                    //метод обратного распостранения ошибки, умножаем ошибку на вес, получаем ошибку нейрона
-                    error_neuron[i][j] += error_neuron[i + 1][k] * overallLayer[i + 1][k][j];
-                }
-            }
-
-
-        for (int i = 1; i < overallLayer.length; i++) // добавить нейрон смещения и дельту весов
-            for (int j = 0; j < overallLayer[i].length; j++) {
-                for (int k = 0; k < overallLayer[i][j].length - 1; k++) {
-                    //по формуле произведения производной сигмоиды, на выход предыдущего слоя и ошибки плюс изменение весов (дельта весов) на предыдущей итерации помноженое на момент
-                    overallLayer[i][j][k] += learning_rate * error_neuron[i][j] * outputs_layer[i][j] * (1 - outputs_layer[i][j]) * outputs_layer[i - 1][k] + deltaOverallLayer[i][j][k] * moment;
-                    //запоминаем их тут, чтобы использовать на следующей итерации
-                    deltaOverallLayer[i][j][k] = learning_rate * error_neuron[i][j] * outputs_layer[i][j] * (1 - outputs_layer[i][j]) * outputs_layer[i - 1][k];
-                }
-                // корректируем весы нейрона смещения
-                overallLayer[i][j][overallLayer[i][j].length - 1] += learning_rate * error_neuron[i][j] * outputs_layer[i][j] * (1 - outputs_layer[i][j]) * 1 + deltaOverallLayer[i][j][deltaOverallLayer[i][j].length - 1] * moment;
-                //запоминаем их тут, чтобы использовать на следующей итерации
-                deltaOverallLayer[i][j][deltaOverallLayer[i][j].length - 1] = learning_rate * error_neuron[i][j] * outputs_layer[i][j] * (1 - outputs_layer[i][j]) * 1;
-            }
-    }
-
-    private float[] sigmoid_mapper(float[] inputs) {
-        float[] outputs = new float[inputs.length];
-        for (int i = 0; i < inputs.length; i++) outputs[i] = sigmoid(inputs[i]);
-        return outputs;
-    }
-
-    private float sigmoid(float x) {
-        return (float) (1 / (1 + Math.exp(-x)));
-    }
-
-    private float[] multiplyByMatrix(float[][] m1, float[] m2) { //2 version with neuron bias, 1 version above
-        float[] overallM = new float[m1.length];
-        for (int y = 0; y < m1.length; y++) {
-            for (int x = 0; x < m1[y].length - 1; x++) overallM[y] += m1[y][x] * m2[x];
-            overallM[y] += m1[y][m1[y].length - 1] * 1;
-        }
-        return overallM;
     }
 }
 
