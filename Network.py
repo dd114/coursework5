5897d76c882c1cd35b0b8ee529aff7d27cffd43e
dd114 <40179423+dd114@users.noreply.github.com>
2021-12-19 22:18:52 +0500

    added: drawing digits

--- Network.py
+++ Network.py
@@ -61,8 +61,8 @@ class Network(object):
             for mini_batch in mini_batches:
                 self.update_mini_batch(mini_batch, eta)
             if test_data:
-                print( "Epoch {0}: {1} / {2}".format(
-                    j, self.evaluate(test_data), n_test))
+                print( "Epoch {0}: {1} % right answers".format(
+                    j, int(self.evaluate(test_data) / n_test * 100)))
             else:
                 print( "Epoch {0} complete".format(j))
 
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from Network import Network
 
 # Third-party libraries
 import numpy as np
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 
 # Press the green button in the gutter to run the script.
@@ -22,8 +22,16 @@ if __name__ == '__main__':
     MNN = Network([784, 30, 10])
     MNN.SGD(training_data, 1, 10, 3.0, test_data = test_data)
 
-    print(np.argmax(MNN.feedforward(test_data[0][0])))
-    print(test_data[0][1])
+    # digit in array for recognition
+    index_digit_in_array = 3
+    probability = MNN.feedforward(validation_data[index_digit_in_array][0])
+    predict_digit = np.argmax(probability)
+    precise_digit = validation_data[index_digit_in_array][1];
+    print("Predict digit = {0}".format(predict_digit))
+    print("Precise digit = {0} with a probability of = {1} %".format(precise_digit, int(probability[predict_digit][0] * 100)))
+
+    plt.imshow(validation_data[index_digit_in_array][0].reshape(28, 28), cmap=plt.cm.binary)
+    plt.show()
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
