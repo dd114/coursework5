34f4df4594db1f0af644e0bd393fb090ac11f84a
dd114 <40179423+dd114@users.noreply.github.com>
2021-12-23 07:22:23 +0500

    added: some corrections

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import mnist_loader
 from Network import Network
 
@@ -20,17 +16,19 @@ if __name__ == '__main__':
     # print(len(test_data[0]))
 
     MNN = Network([784, 30, 10])
-    MNN.SGD(training_data, 1, 10, 3.0, test_data = test_data)
+    MNN.SGD(training_data, 1, 10, 3, test_data = test_data)
 
     # digit in array for recognition
-    index_digit_in_array = 3
+    index_digit_in_array = 9
     probability = MNN.feedforward(validation_data[index_digit_in_array][0])
     predict_digit = np.argmax(probability)
+    # print(probability)
     precise_digit = validation_data[index_digit_in_array][1];
     print("Predict digit = {0}".format(predict_digit))
-    print("Precise digit = {0} with a probability of = {1} %".format(precise_digit, int(probability[predict_digit][0] * 100)))
+    print("Precise digit = {0} with a probability of = {1} %".format(precise_digit, int(probability[precise_digit][0] * 100)))
 
     plt.imshow(validation_data[index_digit_in_array][0].reshape(28, 28), cmap=plt.cm.binary)
+    plt.title("This number is {0} with a probability of = {1} %".format(predict_digit, int(probability[predict_digit][0] * 100)))
     plt.show()
 
 
